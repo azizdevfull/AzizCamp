fcc7e444976eab4a71382e5fa2357af5ce78e83a
azizdevfull <aziz16110904@gmail.com>
2022-09-01 21:18:10 +0500

    added time zone

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,33 @@
 class ApplicationController < ActionController::Base
+  # before_action :set_user_time_zone
   before_action :configure_permitted_parameters, if: :devise_controller?
+  around_action :set_user_time_zone, if: :current_user
+
+private 
+
+def set_user_time_zone
+  Time.use_zone(current_user.time_zone) { yield }
+end
 
   protected
-
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [:username, :email, :time_zone, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: [:email, :password]
-    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    devise_parameter_sanitizer.permit :account_update, keys: [:time_zone]
   end
   attr_writer :email
 
   def email
     @email || self.username || self.email
   end
+
+  # def set_user_time_zone
+  #   Time.zone = current_user.time_zone if user_signed_in?
+  # end
+# private
+
+# def set_user_time_zone
+#   Time.zone = current_user.time_zone if user_signed_in?
+# end
 end
