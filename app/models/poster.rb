ccfe46ec0fcd673df322af0f805bc8b26022aabb
Student astrum <student@astrum.domain>
2022-09-02 12:04:30 +0500

    chnge some features

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -2,6 +2,8 @@ class Poster < ApplicationRecord
     belongs_to :user
     has_many :pcomments, dependent: :destroy
     has_one_attached :image
+    has_noticed_notifications model_name: 'Notification'
+    has_many :notifications, through: :user, dependent: :destroy
     # validate :company_logo?
 
     # private
@@ -14,12 +16,11 @@ class Poster < ApplicationRecord
     # validates :image, dimension: { width: { in: 80..100 }, message: 'is not given between dimension' }
     # validates :image, attached: true, content_type: { in: 'application/jpg', message: 'is not a Image' }
     # validate :validate_attachment_filetypes
-    has_noticed_notifications model_name: 'Notification'
-    has_many :notifications, through: :user, dependent: :destroy
+
     # private
     # def validate_attachment_filetypes
     #     return unless rasim.attached?
-    
+
     #     rasim.each do |attachment|
     #       unless attachment.content_type.in?(%w[image/jpeg image/png image/gif video/mp4 video/mpeg audio/x-wav audio/ogg])
     #         errors.add(:rasim, 'must be a JPEG, PNG, GIF, MP4, MP3, OGG, or WAV file')
